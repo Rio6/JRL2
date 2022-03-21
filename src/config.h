64ebed99ffed02fc84ee732a218318e61843744f
Rio Liu <rio.liu@r26.me>
2022-03-21 13:32:32 -0400

    update calibraion points

--- src/config.h
+++ src/config.h
@@ -16,5 +16,5 @@ static constexpr int numSamples = 200;
 static constexpr int refreshInterval = 500; // ms
 static constexpr int sampleFreq = 500;
 
-static const float calPoints[] = { 0, 10, 15, 20, 30, 40, 60, 80, 100 };
+static const float calPoints[] = { 15, 20, 25, 30, 40, 50, 60, 80, 100 };
 static constexpr int numCals = sizeof(calPoints) / sizeof(calPoints[0]);
